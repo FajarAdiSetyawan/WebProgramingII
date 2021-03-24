03dd83f0d56e7cfe5615e15fad8994df4ac3b38c
FajarAdiSetyawan <fajaras465@gmail.com>
2021-03-24 11:10:24 +0700

    first commit

---
+++ application/views/pertemuan2.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$nama = "Fajar Adi Setyawan";
+$nim = "NIM: 12180016";
+$kelas = "Kelas: 12.6B.05";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Biodata</title>
+</head>
+
+<body>
+
+  <h1>Biodata</h1>
+  <h4><?= $nama; ?></h4>
+  <h4><?= $nim; ?></h4>
+  <h4><?= $kelas; ?></h4>
+
+</body>
+
+</html>
